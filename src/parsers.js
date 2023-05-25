2eb1746d7fbc03bc2d6778c7eac91f86da190ed0
Vladislav Mayakin <vladislavmayakin@gmail.com>
2023-05-25 20:31:36 +0300

    Add: support for yaml files

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,12 @@
+import yaml from 'js-yaml';
+
 export default (data, extension) => {
   switch (extension) {
     case 'json':
       return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
     default:
       throw new Error(`${extension} is not defined`);
   }
